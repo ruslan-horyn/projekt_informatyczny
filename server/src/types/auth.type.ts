e911fe9bec194851de095e26705f22db69658427
Ruslan Horyn <ruslan.horyn95@gmail.com>
2022-04-25 22:32:51 +0200

    add RequestWithLoginBody

--- server/src/types/auth.type.ts
+++ server/src/types/auth.type.ts
@@ -4,3 +4,10 @@ import { UserI } from './user.type';
 export interface RequestWithUser extends Request {
   user?: UserI;
 }
+
+export interface RequestWithLoginBody extends Request {
+  body: {
+    email: string,
+    password: string
+  };
+}
